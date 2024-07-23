914ce30e80167ebd937a52662f4d4c46529689fa
ultimatemagic79 <sea.starry.ks@gmail.com>
2024-07-23 13:30:25 +0900

    update: response api

--- response.py
+++ response.py
@@ -1,20 +1,24 @@
+from typing import List
+
 import arxiv
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
-def fetch_arxiv_papers(search_query):
-    search = arxiv.Search(query=search_query, max_results=5, sort_by=arxiv.SortCriterion.SubmittedDate)
+def fetch_arxiv_papers(search_query: str, keys: List[str]):
+    client = arxiv.Client()
+    search = arxiv.Search(query=search_query, max_results=100, sort_by=arxiv.SortCriterion.SubmittedDate)
     papers = []
-    for result in search.results():
-        papers.append({"title": result.title, "summary": result.summary, "link": result.pdf_url})
+    for result in client.results(search):
+        if any(key in result.title.lower() for key in keys):
+            papers.append({"title": result.title, "summary": result.summary, "link": result.pdf_url})
     return papers
 
 
 @app.get("/arxiv")
 def get_papers(search_query: str):
-    papers = fetch_arxiv_papers(search_query)
+    papers = fetch_arxiv_papers(search_query, keys)
     return {"papers": papers}
 
 
